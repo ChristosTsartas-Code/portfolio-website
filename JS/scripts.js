aa656993207a061b27ad7b872d6d64a2b6f5e368
ChristosTsartas-Code <tsartakos@gmail.com>
2023-04-21 21:39:05 +0300

    Task 2.3

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,12 @@
 console.log("Hi! Thank you for visiting my Portfolio site")
+console.log("Hi! Thank you for visiting my Portfolio site");
+
+// Responsive menu script.
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
