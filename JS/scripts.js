fbea2abfe38af1d9eefa3c1afa4e4a7aacfe6560
ChristosTsartas-Code <tsartakos@gmail.com>
2023-05-06 17:29:06 +0200

    Changes for 2.5 task

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,9 @@
+
 console.log("Hi! Thank you for visiting my Portfolio site")
-console.log("Hi! Thank you for visiting my Portfolio site");
 
 // Responsive menu script.
-function menuToggle() {
+function menuToggle() 
+{
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
       x.className += ' responsive';
